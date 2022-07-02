ceca090637bc1e90b719050e83a7d3a1d7d556e4
micrab001 <micrab@bk.ru>
2022-07-02 22:59:42 +0300

    сбер - коды Бин исключения добавлены склад - выбор директории, где лежат каталоги по месяцам

--- Сбер.py
+++ Сбер.py
@@ -6,13 +6,26 @@ from tkinter import filedialog
 filename = filedialog.askopenfilename(initialdir=getcwd())
 
 start = time.time()
-# это исключения из сбера, хотя они по бинам подходят
-bin_banks_not_sber = ["417367","422838", "431393", "531317", "531344", "533206", "533681", "539013", "545182", "446942", "446915",
-                      "557029", "557030", "557057", "557071", "557072", "557073", "476208", "676261", "485467", "533616"]
 
-# три цифры, начиная со ВТОРОЙ! здесь коды сбера
-bin_banks = ["006", "173", "228", "274", "276", "279", "308", "313", "332", "336", "369", "381", "390", "451", "469", "479",
-             "484", "570", "762", "817", "854", "0220"]
+with open("Сбер_data.dat") as file:
+    # это исключения из сбера, хотя они по бинам подходят
+    bin_banks_not_sber = eval(file.readline())
+    # три цифры, начиная со ВТОРОЙ! (для МИР начиная с третьей и 4 цифры) здесь коды сбера
+    bin_banks = eval(file.readline())
+
+with open(f"Сбер_data{int(start)}.dat", "w") as file:
+    # запись исключений из сбера, хотя они по бинам подходят и три цифры, начиная со ВТОРОЙ! (для МИР начиная с третьей и 4 цифры) здесь коды сбера
+    file.writelines([str(bin_banks_not_sber)+"\n", str(bin_banks)])
+
+
+
+print(f"Считали коды БИН Сбера и исключения, время {int(time.time() - start)}")
+
+# bin_banks_not_sber = ["417367","422838", "431393", "531317", "531344", "533206", "533681", "539013", "545182", "446942", "446915",
+#                       "557029", "557030", "557057", "557071", "557072", "557073", "476208", "676261", "485467", "533616"]
+#
+# bin_banks = ["006", "173", "228", "274", "276", "279", "308", "313", "332", "336", "369", "381", "390", "451", "469", "479",
+#              "484", "570", "762", "817", "854", "0220"]
 
 def convert_data(val):
     return val.date()
@@ -42,6 +55,32 @@ print(f"обработка колонок завершена, время {int(ti
 df.insert(df.columns.get_loc('Дата операции')+1,'Дата операции магазин', df["Дата операции"].apply(convert_data))
 print(f"добавили дату операции в магазине, время {int(time.time() - start)}")
 df.insert(df.columns.get_loc('Продукт')+1,'BIN', df["Номер карты"].apply(convert_bin))
+
+# проверяем совпадение кодов сбера и исключений по названиям карт от сбера
+df_chk_bin = df[(df["BIN"] == "Другой") & (~df["Продукт"].str.contains("OTHER", regex=False))]
+if len(df_chk_bin) > 0:
+    # добавить в bin сбера коды
+    card_numb = df_chk_bin["Номер карты"].tolist()
+    for el in card_numb:
+        if el[0:2] == "22": # проверка карт мир
+            bin_banks.append(el[2:6])
+        else:
+            bin_banks.append(el[1:4])
+    bin_banks.sort()
+    print(f"Найдено {len(df_chk_bin)} новых кодов BIN Сбера")
+else:
+    print("Новых кодов BIN Сбера в таблице не найдено")
+
+df_chk_bin = df[(df["BIN"] == "Сбербанк") & (df["Продукт"].str.contains("OTHER", regex=False))]
+if len(df_chk_bin) > 0:
+    card_numb = df_chk_bin["Номер карты"].tolist()
+    for el in card_numb:
+        bin_banks_not_sber.append(el[0:6])
+    bin_banks_not_sber.sort()
+    print(f"Найдено {len(df_chk_bin)} новых исключений кодов BIN Сбера")
+else:
+    print("Новых исключений кодов BIN Сбера в таблице не найдено")
+
 print(f"обработали номера карт на предмет карт Сбера, время {int(time.time() - start)}")
 
 banks_data = pd.pivot_table(df,index=['BIN'], values=['Сумма операции'], aggfunc=[sum,len]) # margins=True
@@ -83,9 +122,13 @@ with writer as file_name:
     worksheet.set_column('E:E', 10, format2)
 
     # writer.save()
-
-
-
 print(f"записали в файл сводную таблицу, время {int(time.time() - start)}")
+
+with open("Сбер_data.dat", "w") as file:
+    # запись исключений из сбера, хотя они по бинам подходят и три цифры, начиная со ВТОРОЙ! (для МИР начиная с третьей и 4 цифры) здесь коды сбера
+    file.writelines([str(bin_banks_not_sber)+"\n", str(bin_banks)])
+
+print(f"Записали коды БИН Сбера и исключения, время {int(time.time() - start)}")
+
 end = time.time()
 print(f"программа выполнена за {int(end - start)} секунд, записан файл 222.xlsx")
